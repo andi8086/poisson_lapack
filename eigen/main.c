2a1ab1d5347b9e64fcf535dd35394b7c65a6a7a7
Andreas J. Reichel <andreas@reichel.bayern>
2023-09-27 11:54:11 +0200

    Refactor code and fix bug with blas/blas64 mishmash
    Somehow I can only link to openblas, not openblas64.
    If I do the latter, eigenvector calculation
    corrupts the stack.
    
    Signed-off-by: Andreas J. Reichel <andreas@reichel.bayern>

--- eigen/main.c
+++ eigen/main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <openblas64/lapacke.h>
+#include <openblas64/cblas.h>
 #include <string.h>
 #include <stdlib.h>
 
-#define N 4
+/* one side of the square */
+#define X 48 
+/* 2D laplace matrix has X^4 elements (N*N)*/
+#define N (X*X)
 
-void print_matrix(lapack_complex_double *m)
+
+#define CDOUBLE lapack_complex_double
+
+void print_matrix(lapack_complex_double *m, size_t rr )
 {
         printf("\n");
-        for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                        printf("%.5f + %.5fi   ", creal(m[i*N + j]), cimag(m[i*N + j]));
+        for (size_t i = 0; i < rr; i++) {
+                for (size_t j = 0; j < rr; j++) {
+                        printf("%.2f + %.2fi  ", creal(m[i*N + j]), cimag(m[i*rr + j]));
                 }
                 printf("\n");
         }
@@ -18,13 +25,26 @@ void print_matrix(lapack_complex_double *m)
 }
 
 
-void matrix_mul(lapack_complex_double *c, lapack_complex_double *a, lapack_complex_double *b)
+void print_matrix_real(lapack_complex_double *m, size_t rr )
 {
-        for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) { 
-                        c[i*N + j] = 0.0;
-                        for (int k = 0; k < N; k++) {
-                                c[i*N + j] += a[i*N + k] * b[k*N + j];
+        printf("\n");
+        for (size_t i = 0; i < rr; i++) {
+                for (size_t j = 0; j < rr; j++) {
+                        printf("%+1.0f  ", creal(m[i*N + j]));
+                }
+                printf("\n");
+        }
+        printf("\n");
+}
+
+
+void matrix_mul(lapack_complex_double *c, lapack_complex_double *a, lapack_complex_double *b, size_t rr)
+{
+        for (size_t i = 0; i < rr; i++) {
+                for (size_t j = 0; j < rr; j++) { 
+                        c[i*rr + j] = 0.0;
+                        for (size_t k = 0; k < rr; k++) {
+                                c[i*rr + j] += a[i*rr + k] * b[k*rr + j];
                         }
                 }
         }
@@ -32,23 +52,162 @@ void matrix_mul(lapack_complex_double *c, lapack_complex_double *a, lapack_compl
 }
 
 
-void matrix_adj(lapack_complex_double *c, lapack_complex_double *a)
+void matrix_adj(lapack_complex_double *c, lapack_complex_double *a, size_t rr)
 {
-        for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                        c[i*N + j] = conj(a[j*N + i]);
+        for (size_t i = 0; i < rr; i++) {
+                for (size_t j = 0; j < rr; j++) {
+                        c[i*rr + j] = conj(a[j*rr + i]);
                 }
         }
 }
 
 
-void mat_v(lapack_complex_double *b, lapack_complex_double *m, lapack_complex_double *a)
+void mat_v(lapack_complex_double *b, lapack_complex_double *m, lapack_complex_double *a,
+           size_t rr)
 {
-        for (int i = 0; i < N; i++) {
+        for (size_t i = 0; i < rr; i++) {
                 b[i] = 0.0;
 
-                for (int j = 0; j < N; j++) {
-                        b[i] += m[i*N + j]*a[j];
+                for (size_t j = 0; j < rr; j++) {
+                        b[i] += m[i*rr + j]*a[j];
+                }
+        }
+}
+
+
+CDOUBLE *malloc_cmatrix(size_t dim)
+{
+        CDOUBLE *tmp = malloc(sizeof(CDOUBLE) * dim * dim);
+        if (!tmp) {
+                perror("Out of memory\n");
+                exit(-1);
+        }
+        return tmp;
+}
+
+
+CDOUBLE *malloc_cvector(size_t dim)
+{
+        CDOUBLE *tmp = malloc(sizeof(CDOUBLE) * dim);
+        if (!tmp) {
+                perror("Out of memory\n");
+                exit(-1);
+        }
+        return tmp;
+}
+
+
+/* input: dim: dimension of m
+          m: hermitian matrix, with NxN elements
+   output: Q: transformation matrix
+           d: tridiagonal main diagonal elements, N elements
+           e: tridiagonal extra diagonal elements, N elements
+           ev: NxN matrix with columns = eigenvectors
+           l: eigenvalues
+           n_ev: found eigen values */
+int eigenv(size_t dim, CDOUBLE *m, CDOUBLE *Q, double *d,
+           double *e, CDOUBLE *ev, double *l,
+           lapack_int *n_ev, lapack_int *issupz,
+           lapack_int i_start, lapack_int i_end)
+{
+        const size_t mat_size = sizeof(lapack_complex_double) * dim * dim;
+
+        CDOUBLE *tau = malloc_cvector(dim-1);
+        CDOUBLE *v = malloc_cvector(dim);
+        CDOUBLE *H = malloc_cmatrix(dim);
+        CDOUBLE *tmp = malloc_cmatrix(dim);
+
+        int info = LAPACKE_zhetrd(LAPACK_ROW_MAJOR, 'U', dim, m, dim, d, e, tau);
+        if (info) {
+                printf("Tridiagonalization failed.\n");
+                free(tmp);
+                free(H);
+                free(v);
+                free(tau);
+                return info;
+        }
+
+        printf("Tridiagonalization completed.\n");
+
+        memset(Q, 0, mat_size);
+        for (size_t i = 0; i < dim; i++) {
+                Q[i*dim + i] = 1.0;        
+        }
+        for (size_t i = 0; i < dim - 1; i++) {
+                printf("Calculating Q: %02.0f%%\r", 100.0/(dim-1)*i);
+                fflush(stdout);
+                for (size_t j = i+1; j < dim; j++) {
+                        v[j] = 0.0;
+                }
+                for (size_t j = 0; j < i; j++) {
+                        v[j] = m[j * dim + i+1];
+                }
+                v[i] = 1.0;
+
+                /* Calculate H_i = I - tau * v * v**H */
+                for (size_t j = 0; j < dim; j++) {
+                        for (size_t k = 0; k < dim; k++) {
+                                if (k == j) {
+                                        H[j*dim + k] = 1.0;
+                                } else {
+                                        H[j*dim + k] = 0.0;
+                                }
+                                H[j*dim + k] -= tau[i] * v[j] * conj(v[k]);
+                        }
+                }
+                /* Calculate Q' = H_i * Q */
+                // matrix_mul(tmp, H, Q, N);
+                lapack_complex_double alpha = 1.0;
+                lapack_complex_double beta = 0.0;
+                memcpy(tmp, Q, mat_size);
+                cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                            dim, dim, dim, &alpha,
+                            H, dim, tmp, dim, &beta, Q, dim);
+        }
+        printf("\n");
+        info = LAPACKE_zstegr(LAPACK_ROW_MAJOR,
+                              'V', 'A',
+                              dim, d, e,
+                              0.0, 0.0,
+                              i_start, i_end,
+                              0.0,
+                              n_ev, l, ev,
+                              dim, issupz);
+
+        free(tmp);
+        free(H);
+        free(v);
+        free(tau);
+        return 0;
+}
+
+
+void laplace2d(size_t rr, size_t block_len, CDOUBLE *mat)
+{
+        /* the matrix is complex, we only set real parts here */
+
+        memset(mat, 0, rr*rr*sizeof(CDOUBLE));
+        for (size_t k = 0; k < rr; k+=block_len) {
+                /* inner blocks */
+                for (size_t l = 0; l < block_len; l++) {
+                        if (l > 0) {
+                                mat[(k+l)*rr + (k+l-1)] = -1.0;
+                        }
+                        /* diagonal of inner B matrix */
+                        mat[(k+l)*rr + (k+l)] = 4.0;
+                        /* upper tridiagonal elements of inner B matrix */
+                        if (l < block_len - 1) {
+                                mat[(k+l)*rr + (k+l+1)] = -1.0;
+                        }
+                }
+                /* upper pentadiagonal band */
+                for (size_t l = 0; l < block_len; l++) {
+                        if (k >= block_len) {
+                                mat[(k+l)*rr + (k+l-block_len)] = -1.0; 
+                        }
+                        if (k < rr - block_len) {
+                                mat[(k+l)*rr + (k+l+block_len)] = -1.0;
+                        }
                 }
         }
 }
@@ -57,148 +216,61 @@ void mat_v(lapack_complex_double *b, lapack_complex_double *m, lapack_complex_do
 int main(void)
 {
 
-        lapack_complex_double *m;
+        CDOUBLE *m = malloc_cmatrix(N);
+        laplace2d(N, X, m);
 
-        m = malloc(sizeof(lapack_complex_double) * N * N);
+       // CDOUBLE *m_old = malloc_cmatrix(N);
+        //memcpy(m_old, m, sizeof(lapack_complex_double) * N * N);
 
-        m[0] = 2.3;
-        m[1] = lapack_make_complex_double(1.0, -3.3);
-        m[2] = lapack_make_complex_double(0, -2.0);
-        m[3] = 1.7;
-
-        m[4] = lapack_make_complex_double(1.0, 3.3);
-        m[5] = 0.5;
-        m[6] = 7.0;
-        m[7] = 0.0;
-
-        m[8] = lapack_make_complex_double(0, 2.0);
-        m[9] = 7.0;
-        m[10] = 0.5;
-        m[11] = -1.0;
-
-        m[12] = 1.7;
-        m[13] = 0.0;
-        m[14] = -1.0;
-        m[15] = 3.0;
-
-        lapack_complex_double *m_old = malloc(sizeof(lapack_complex_double) * N * N);
-        memcpy(m_old, m, sizeof(lapack_complex_double) * N * N);
-
-        /* A is Hermitian */
-        /* find T so that Q**H * A * Q = T and T is real sym tridiag */
-        double *d = malloc(sizeof(double) * (N));
-
+        double *d = malloc(sizeof(double) * N);
         /* we need N-1 elements for zhetrd, but one additional for zstegr */
-        double *e = malloc(sizeof(double) * (N));
+        double *e = malloc(sizeof(double) * N);
 
-        lapack_complex_double *tau = malloc(sizeof(lapack_complex_double) * (N-1));
-        printf("******************");
-        print_matrix(m);
-        printf("******************");
+        lapack_int *issupz = malloc(sizeof(lapack_int) * 2 * N);
 
-        int info = LAPACKE_zhetrd(LAPACK_ROW_MAJOR, 'U', N, m, N, d, e, tau);
-        printf("zhetrd returned %d\n", info);
-
-        printf("******************");
-        print_matrix(m);
-        printf("******************");
-
-        /* calculate H(i) = I - tau * v * v**H */
-
-        lapack_complex_double *v = malloc(sizeof(lapack_complex_double) * N);
-        lapack_complex_double **H = malloc(sizeof(void *)*(N-1));
-        for (int i = 0; i < N-1; i++) {
-                H[i] = malloc(sizeof(lapack_complex_double) * N * N);
-                for (int j = i+1; j < N; j++) {
-                        v[j] = 0.0;
-                }
-                for (int j = 0; j < i; j++) {
-                        v[j] = m[j * N + i+1];
-                }
-                v[i] = 1.0;
-                printf("v_%d = [%.5f + %.5fi, %.5f + %.5fi, %.5f + %.5fi, "
-                                "%.5f + %.5fi]**T\n", i,
-                     creal(v[0]), cimag(v[0]),
-                     creal(v[1]), cimag(v[1]),
-                     creal(v[2]), cimag(v[2]),
-                     creal(v[3]), cimag(v[3]));
-
-                /* Calculate I - tau * v * v**H */
-                for (int j = 0; j < N; j++) {
-                        for (int k = 0; k < N; k++) {
-                                if (k == j) {
-                                        H[i][j*N + k] = 1.0;
-                                } else {
-                                        H[i][j*N + k] = 0.0;
-                                }
-                                H[i][j*N + k] -= tau[i] * v[j] * conj(v[k]);
-                        }
-                }
-                printf("tau_%d = %.5f + %.5fi\n", i, creal(tau[i]), cimag(tau[i]));
-                printf("H_%d:\n", i);
-                print_matrix(H[i]);
-        }
-
-        lapack_complex_double *Q = malloc(sizeof(lapack_complex_double) * N * N);
-        lapack_complex_double *tmp = malloc(sizeof(lapack_complex_double) * N * N);
-        /* create Q = H(2) H(1) H(0) */
-        matrix_mul(tmp, H[1], H[0]);
-        matrix_mul(Q, H[2], tmp);
-        print_matrix(Q);
-
-        /* calculate Q * T * Q**H */
-
-        lapack_complex_double *T = malloc(sizeof(lapack_complex_double)*N*N);
-        memset(T, 0, sizeof(lapack_complex_double) * N * N);
-        for (int i = 0; i < N; i++) {
-                T[i*N + i] = d[i]; 
-                if (i < N-1) {
-                        T[i*N + i + 1] = e[i];
-                        T[(i+1)*N + i] = e[i];
-                }
-        }
-
-        printf("*****************************\n");
-        printf("Tridiagonal matrix:\n");
-        print_matrix(T);
-
-        matrix_adj(H[0], Q);
-        matrix_mul(tmp, T, H[0]);
-        matrix_mul(T, Q, tmp);
-
-        printf("*****************************\n");
-        printf("Q T Q^H:\n");
-        print_matrix(T);
-
-        printf("*****************************\n");
-        printf("Original matrix:\n");
-        print_matrix(m_old);
-
+        double *eigen_values = malloc(sizeof(double) * N);
         lapack_int num_eigenvals;
-        double eigen_values[N];
-        lapack_int issupz[2*N];
-        lapack_complex_double *eigen_vecs = malloc(sizeof(lapack_complex_double) * N * N);
-        /* Now we can calculate the eigen values and eigen vectors */
-        
-        int res = LAPACKE_zstegr(LAPACK_ROW_MAJOR, 'V', 'A', N, d, e, 0.0, 0.0,
-        0, 0, 0.0, &num_eigenvals, eigen_values, eigen_vecs, N, issupz);
-        printf("zstegr returned %d\n", res);
 
+        CDOUBLE *eigen_vecs = malloc_cmatrix(N);
+        /* Now we can calculate the eigen values and eigen vectors */
+        CDOUBLE *Q = malloc_cmatrix(N); 
+
+        eigenv(N, m, Q, d, e, eigen_vecs, eigen_values, &num_eigenvals, issupz,
+               0, 0);
+        
         printf("%d eigen values found.\n", num_eigenvals);
+
+        free(eigen_vecs);
+
+//        free(m_old);
+//        free(tmp);
+        free(Q);
+        free(eigen_values);
+        free(issupz);
+        free(e);
+        free(d);
+        free(m);
+
+        return 0;
+}
+        
+
+
+
 
         /* Retransform eigen vectors with Q to match base of A */
         /* Q T Q^H Q v = A Q v = lambda Q v,
            hence  v' = Qv */
-
-
+/*
+        lapack_complex_double *tmp = malloc(sizeof(lapack_complex_double) * N * 2);
         for (int i = 0; i < N; i++) {
                 printf("lambda_%d = %.5f\n", i, eigen_values[i]);
 
-                /* store column i of eigen_vecs into row 2 of tmp */
+                //  store column i of eigen_vecs into row 2 of tmp
                 for (int j = 0; j < N; j++) {
                         tmp[N + j] = eigen_vecs[j*N + i];
                 }
-                mat_v(tmp, Q, &tmp[N]);
+                mat_v(tmp, Q, &tmp[N], N);
 
                 printf("ev_%d = [ ", i);
                 for (int j = 0; j < N; j++) {
@@ -210,9 +282,8 @@ int main(void)
                 }
                 printf(" ]\n");
 
-                /* Test eigenvector */
-
-                mat_v(&tmp[N], m_old, tmp);
+                // Test eigenvectors
+                mat_v(&tmp[N], m_old, tmp, N);
                 printf("A ev_%d / lambda_%d = [ ", i, i);
                 for (int j = 0; j < N; j++) {
                         printf("%.5f + %.5fi", creal(tmp[N+j])/eigen_values[i],
@@ -224,27 +295,4 @@ int main(void)
                 printf(" ]\n");
 
         }
-
-
-        free(eigen_vecs);
-
-        free(T);
-        free(m_old);
-        free(tmp);
-        free(Q);
-
-        for (int i = 0; i < N-1; i++) {
-                free(H[i]);
-        }
-        free(H);
-
-        free(v);
-
-        free(tau);
-
-        free(e);
-        free(d);
-        free(m);
-
-        return 0;
-}
+*/
